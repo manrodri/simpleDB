c3cd82596ebc6cbf94b205e3136fdbd04e5769f2
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2019-07-06 17:19:00 +0100

    test

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  profile    = "default"
+  region = "eu-west-1"
+ 
+}
+resource "aws_instance" "staging_server" {
+
+  ami = "ami-0a5efcfd56b6983c4"
+  instance_type = "t2.micro"
+  key_name      = "manuel_tech_case"
+  private_ip = "192.168.1.240"
+  vpc_security_group_ids = ["sg-0b0afa51d47dcad45"]
+  subnet_id = "subnet-0f97221768fbbfa7c"
+
+  connection {
+    type     = "ssh"
+    user     = "jenkins"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+  }
+
+  provisioner "file" {
+    source      = "../run.py"
+    destination = "/tmp/run.py"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "python2 /tmp/run.py 3000",
+      
+    ]
+  }
+
+  tags = {
+    Name = "server deployed by Terraform"
+    owner= "manuel"
+    provisioned="terraform"
+  }
+}
---
+++ terraform/var.tf
@@ -0,0 +1,3 @@
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "/var/lib/jenkins/.ssh/id_rsa"
+}
